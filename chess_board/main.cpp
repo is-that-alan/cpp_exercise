8057d8e293d8c37d1d7657011b14fa933615d0d6
Alan Wong <thw.alanwong@gmail.com>
2023-07-23 21:55:44 +0800

    Update pritnBoard function

--- chess_board/main.cpp
+++ chess_board/main.cpp
@@ -4,10 +4,15 @@
 const int ROWS = 8;
 const int COLS = 8;
 
-namespace queen_attack{
-    const auto make_pair(int x, int y){
+namespace queen_attack {
+    const auto make_pair(int x, int y) {
 
     }
+
+    bool can_attack(int arr[ROWS][COLS]) {
+
+    }
+
     class ChessBoard {
     private:
         const int SIZE = 8;
@@ -15,24 +20,23 @@ namespace queen_attack{
 
     public:
         ChessBoard() {
-            board.resize(SIZE, std::vector<char>(SIZE,' '));
+            board.resize(SIZE, std::vector<char>(SIZE, ' '));
         }
-        void printBoard() const{
-            for (int i=0;i<SIZE; i++){
-                for (int j=0; j<SIZE; j++){
-                    std::cout << board[i][j] << " ";
+
+        void printBoard() const {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    std::cout << board[i][j] << "|_ ";
                 }
                 std::cout << std::endl;
             }
         }
     };
-    bool can_attack(int arr[ROWS][COLS]){
-
-    }
 }
 
 int main(){
-    int chessBoard[ROWS][COLS];
+    queen_attack::ChessBoard chessBoard;
+    chessBoard.printBoard();
 
     return 0;
 }
