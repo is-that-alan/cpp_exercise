bc24340549ad6c94c1e7ce4b393790ae0297d446
Alan Wong <thw.alanwong@gmail.com>
2023-07-25 20:47:53 +0800

    Update cpp_ml project

--- cpp_ml/data.hpp
+++ cpp_ml/data.hpp
@@ -14,6 +14,8 @@ class data{
     int enum_label;
 
 public:
+    data();
+    ~data();
     void set_feature_vector(std::vector<uint8_t> *);
     void append_to_feature_vector(uint8_t);
     void set_label(uint8_t);
--- cpp_ml/data_handler.cc
+++ cpp_ml/data_handler.cc
@@ -8,5 +8,46 @@ data_handler::data_handler() {
     validation_data = new std::vector<data *>;
 }
 
-data_handler::~data_handler();
+data_handler::~data_handler(){
+    //FREE Dynamically Allocated memory
 
+};
+
+void data_handler::read_feature_vector(std::string path){
+    uint32_t header[4]; // |MAGIC| NUM IMAGES| ROWSIZE| COLSIZE
+    unsigned char bytes[4]; // allow use to read all 32 bits
+    FILE *f = fopen(path.c_str(), "r");
+    if (f){
+        for (int i=0; i < 4; i++){
+            if(fread(bytes, sizeof(bytes), 1, f)){
+                header[i]  = convert_to_little_endian(bytes);
+            }
+        }
+        printf("Done getting file headers.\n");
+        int image_size = header[2]*header[3]; //ROWSIZE * COLSIZE
+        for (int i=0; i < header[1]; i ++){
+            data *d = new data();
+            uint8_t element[1];
+            for (int j=0; j < image_size; j++) {
+                if (fread(element, sizeof(element), 1, f)) {
+                    d->append_to_feature_vector(element[0]);
+                } else {
+                    printf("Error Reading from file.\n");
+                    exit(1);
+                }
+            }
+        }
+    } else{
+        printf("Could not find file.\n");
+        exit(1);
+    }
+
+};
+void data_handler::read_feature_lables(std::string path);
+void data_handler::split_data();
+void data_handler::count_classes();
+uint32_t data_handler::convert_to_little_endian(const unsigned char* bytes);
+
+std::vector<data *> * data_handler::get_training_data();
+std::vector<data *> * data_handler::get_test_data();
+std::vector<data *> * data_handler::get_validation_data();
