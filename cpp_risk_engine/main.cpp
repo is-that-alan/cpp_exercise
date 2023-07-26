933b676fa4fcd66feb5b6f3c6f1ac92f8640e44f
Alan Wong <thw.alanwong@gmail.com>
2023-07-26 08:51:30 +0800

    Add boost lib

--- cpp_risk_engine/main.cpp
+++ cpp_risk_engine/main.cpp
@@ -1,30 +1,53 @@
 #include <iostream>
 #include <map>
-using std::string, std::map, std::vector;
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
 
-class Constituent{
-private:
-    map <string, vector<double>> priceSeries;
-public:
-    Constituent(string ticker, vector<double> series ){
-        this -> priceSeries[ticker] = series;
-    }
-    map <string, vector<double>> getPriceSeries(){
-        return this -> priceSeries;
-    };
+int main () {
+    using namespace boost::numeric::ublas;
+    matrix<double> m (3, 3);
 
-};
+    // Assign values to the matrix
+    int value = 1;
+    for (unsigned i = 0; i < m.size1 (); ++ i)
+        for (unsigned j = 0; j < m.size2 (); ++ j)
+            m (i, j) = value++;
 
-int main() {
-    vector<double> aaplSeries = {0.1,0.2,.3};
-    Constituent aapl("AAPL", (aaplSeries));
-    auto myPriceSeries = aapl.getPriceSeries();
-    for (const auto& price: myPriceSeries){
-        for (const auto& item: price.second){
-            std::cout << "Price: " << item << "\n";
-        }
-    }
+    std::cout << m << std::endl;
 
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+
+//using std::string, std::map, std::vector;
+//#include <fmt/format.h>
+//#include <fmt/core.h>
+//
+//
+//
+//class Constituent{
+//private:
+//    map <string, vector<double>> priceSeries;
+//public:
+//    Constituent(string ticker, vector<double> series ){
+//        this -> priceSeries[ticker] = series;
+//    }
+//    map <string, vector<double>> getPriceSeries(){
+//        return this -> priceSeries;
+//    };
+//
+//};
+//
+//int main() {
+//    vector<double> aaplSeries = {0.1,0.2,.3};
+//    Constituent aapl("AAPL", (aaplSeries));
+//    auto myPriceSeries = aapl.getPriceSeries();
+//    for (const auto& price: myPriceSeries){
+//        for (const auto& item: price.second){
+//            std::cout << "Price: " << item << "\n";
+//        }
+//    }
+//    fmt::print("The answer is {}.", 42);
+//    std::cout << "Hello, World!" << std::endl;
+//    return 0;
+//}
